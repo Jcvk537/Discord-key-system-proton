792d03254cee131a0f49c1d104f6e5d214a9f673
Jcvk537 <167979973+Jcvk537@users.noreply.github.com>
2025-05-22 12:52:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -75,3 +75,16 @@ async def help_command(ctx):
     await ctx.send(help_msg)
 
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Ciao, Render funziona!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
